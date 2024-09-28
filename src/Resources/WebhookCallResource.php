5cbfe2d5dceca18ea59afe5f8bd8f023f632a4b1
andreia <andreia@users.noreply.github.com>
2024-09-28 21:40:10 +0000

    Fix styling

--- src/Resources/WebhookCallResource.php
+++ src/Resources/WebhookCallResource.php
@@ -11,7 +11,6 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
 use Spatie\WebhookClient\Models\WebhookCall;
-use Tapp\FilamentWebhookClient\Resources\WebhookCallResource\Pages;
 
 class WebhookCallResource extends Resource
 {
