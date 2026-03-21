a5f499d771f830ed2c2e1a768040ebe29446208b
Steve Williamson <steve@sjdub.com>
2026-03-21 13:33:00 -0400

    phpstan

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+// Register the package view namespace for static analysis
+if (function_exists('view')) {
+    app('view')->addNamespace(
+        'filament-webhook-client',
+        __DIR__ . '/resources/views'
+    );
+}
